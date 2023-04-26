0c7d912ffa7a7f14d66de07d9abadf5a170dc858
lobiancovitor <lobiancocnvitor@hotmail.com>
2023-04-26 11:32:04 -0300

    oi

--- Codigo/Codigo_java/src/Departamento.java
+++ Codigo/Codigo_java/src/Departamento.java
@@ -1,17 +1,67 @@
 public class Departamento {
-    // Atributos
     private int id;
     private String nome;
     private String localizacao;
-    private int gerenteId;
+    private int gerente;
 
-    //Método construtor
-
-    Departamento(int id, String nome, String localizacao, int gerenteId){
+    public Departamento(int id, String nome, String localizacao, int gerente) {
         this.id = id;
         this.nome = nome;
         this.localizacao = localizacao;
-        this.gerenteId = gerenteId;
+        this.gerente = gerente;
     }
-    
+
+    public void readDepartamento(Departamento departamento) {
+        System.out.println(departamento);
+    }
+
+    public void updateDepartamento(int id, String nome, String localizacao, int gerente) {
+        this.id = id;
+        this.nome = nome;
+        this.localizacao = localizacao;
+        this.gerente = gerente;
+    }
+
+    @Override
+    public String toString() {
+        return
+            "Id: " + getId() + "\n" +
+            "Nome: " + getNome() + "\n" +
+            "Localização: " + getLocalizacao() + "\n" +
+            "Gerente: " + getGerente()
+            ;
+    }
+
+    public int getId() {
+        return this.id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getNome() {
+        return this.nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getLocalizacao() {
+        return this.localizacao;
+    }
+
+    public void setLocalizacao(String localizacao) {
+        this.localizacao = localizacao;
+    }
+
+    public int getGerente() {
+        return this.gerente;
+    }
+
+    public void setGerente(int gerente) {
+        this.gerente = gerente;
+    }
+
 }
