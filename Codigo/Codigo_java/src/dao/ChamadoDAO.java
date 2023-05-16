5c04118850f44f9d9c813cd060ccd9c0807f7b07
AmandaSenra <senraamanda16@gmail.com>
2023-05-16 20:17:27 -0300

    Update ChamadoDAO

--- Codigo/Codigo_java/src/dao/ChamadoDAO.java
+++ Codigo/Codigo_java/src/dao/ChamadoDAO.java
@@ -100,4 +100,30 @@ public class ChamadoDAO {
         return chamados;
     }
 
+    public boolean update(Chamado chamado) throws SQLException {
+        CriaConexao criaConexao = new CriaConexao();
+        Connection connection = criaConexao.recuperarConexao();
+
+        String sql = "UPDATE chamado SET status = ?, titulo = ?, descricao = ?, id_usuario = ?, nome_solictante = ?, email_solicitante = ?, telefone_solictante = ?, data_abertura = ? WHERE protocolo = ?";
+        try (PreparedStatement pstm = (PreparedStatement) connection.prepareStatement(sql)) {
+            pstm.setInt(1, chamado.getProtocolo());
+            pstm.setString(2, chamado.getStatus());
+            pstm.setString(3, chamado.getTitulo());
+            pstm.setString(4, chamado.getDescricao());
+            pstm.setInt(5, chamado.getIdUsuario());
+            pstm.setString(6, chamado.getNomeSolicitante());
+            pstm.setString(7, chamado.getEmailSolicitante());
+            pstm.setString(8, chamado.getTelefoneSolicitante());
+            pstm.setDate(9, chamado.getDataAbertura());
+            pstm.execute();
+            System.out.println("Chamado atualizado.");
+
+            connection.close();
+
+            return pstm.execute();  
+        }
+    }
+
+
+
 }
