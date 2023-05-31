5f2176c7d1c4977d12320add5a5fdf0e1ff0a8f7
AmandaSenra <senraamanda16@gmail.com>
2023-05-31 10:56:06 -0300

    Criar chamadoControle

--- Codigo/Codigo_java/src/controle/ChamadoControle.java
+++ Codigo/Codigo_java/src/controle/ChamadoControle.java
@@ -1,5 +1,20 @@
 package controle;
+import dao.ChamadoDAO;
+import dao.ConnectionFactory;
+import model.Usuario;
+
+import java.sql.Connection;
+import java.util.ArrayList;
 
 public class ChamadoControle {
-    
+    //lista de chamados
+    private Connection connection;
+
+    public ChamadoControle(Connection connection){
+        ConnectionFactory fabricade
+    }
+
+    public ArrayList<Chamado> getChamadosUsuarios(Usuario A){
+
+    }
 }
